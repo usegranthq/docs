770dfac0f1fb37730faa14534c0668190d8f8b61
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2024-11-17 07:23:50 +0530

    Update TOC layout

--- app/(home)/[[...slug]]/page.tsx
+++ app/(home)/[[...slug]]/page.tsx
@@ -12,7 +12,14 @@ export default async function Page(props: { params: Promise<{ slug?: string[] }>
   const MDX = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}
+      tableOfContent={{
+        style: 'clerk',
+        single: false,
+      }}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
